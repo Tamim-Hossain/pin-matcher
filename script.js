7447096f2e75b9a80e397c43699805d31801970e
“[firstname <tamim_hossain@outlook.com>
2020-08-04 12:11:20 +0600

    almost done

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 document.getElementById("generate-pin-btn").addEventListener("click", function () {
-    const randomPin = Math.round(1000 + Math.random() * 9999);
+    const randomPin = Math.round(1000 + (Math.random() * 9999));
     document.getElementById("generateOutput").value = randomPin;
+});
+
+document.getElementById("submit").addEventListener("click", function () {
+    const generateOutputValue = document.getElementById("generateOutput").value;
+    const numberOutputValue = document.getElementById("numberOutput").value;
+
+    if (generateOutputValue == numberOutputValue) {
+        document.getElementById("wrong").style.display = "none";
+        document.getElementById("correct").style.display = "block";
+    } else {
+        document.getElementById("correct").style.display = "none";
+        document.getElementById("wrong").style.display = "block";
+    }
 });
