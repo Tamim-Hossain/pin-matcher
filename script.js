8d4741e9bcf102fbeea4bd836830bdca5e8b1a38
“[firstname <tamim_hossain@outlook.com>
2020-08-04 16:04:01 +0600

    complete

--- script.js
+++ script.js
@@ -6,12 +6,21 @@ document.getElementById("generate-pin-btn").addEventListener("click", function (
 document.getElementById("submit").addEventListener("click", function () {
     const generateOutputValue = document.getElementById("generateOutput").value;
     const numberOutputValue = document.getElementById("numberOutput").value;
-
     if (generateOutputValue == numberOutputValue) {
-        document.getElementById("wrong").style.display = "none";
-        document.getElementById("correct").style.display = "block";
+        document.getElementById("wrong-alert").style.display = "none";
+        document.getElementById("correct-alert").style.display = "block";
     } else {
-        document.getElementById("correct").style.display = "none";
-        document.getElementById("wrong").style.display = "block";
+        document.getElementById("correct-alert").style.display = "none";
+        document.getElementById("wrong-alert").style.display = "block";
+
+        const submitCount = parseInt(document.getElementById("submit-count").innerText);
+        const submitRemain = document.getElementById("submit-count").innerText = submitCount - 1;
+        if (submitRemain <= 0) {
+            document.getElementById("submit-count").innerText = 3;
+            document.getElementById("submit").disabled = true;
+            document.getElementById("correct-alert").style.display = "none";
+            document.getElementById("wrong-alert").style.display = "none";
+            document.getElementById("limit-alert").style.display = "block";
+        }
     }
 });
