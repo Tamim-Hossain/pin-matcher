4c3ba297078e7b9b5947296ad936afe172c4f49e
“[firstname <tamim_hossain@outlook.com>
2020-08-04 17:00:19 +0600

    code updated

--- script.js
+++ script.js
@@ -7,20 +7,21 @@ document.getElementById("submit").addEventListener("click", function () {
     const generateOutputValue = document.getElementById("generateOutput").value;
     const numberOutputValue = document.getElementById("numberOutput").value;
     if (generateOutputValue == numberOutputValue) {
-        document.getElementById("wrong-alert").style.display = "none";
         document.getElementById("correct-alert").style.display = "block";
+        document.getElementById("wrong-alert").style.display = "none";
     } else {
-        document.getElementById("correct-alert").style.display = "none";
         document.getElementById("wrong-alert").style.display = "block";
+        document.getElementById("correct-alert").style.display = "none";
 
         const submitCount = parseInt(document.getElementById("submit-count").innerText);
         const submitRemain = document.getElementById("submit-count").innerText = submitCount - 1;
         if (submitRemain <= 0) {
             document.getElementById("submit-count").innerText = 3;
             document.getElementById("submit").disabled = true;
+            document.getElementById("limit-alert").style.display = "block";
             document.getElementById("correct-alert").style.display = "none";
             document.getElementById("wrong-alert").style.display = "none";
-            document.getElementById("limit-alert").style.display = "block";
+            document.getElementById("limit").innerText = "Limit exceeded";
         }
     }
 });
